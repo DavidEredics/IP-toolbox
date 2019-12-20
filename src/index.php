41ce5cb0c0be82bef688dcebcd24e781774ab6ba
Dávid <mail@davideredics.com>
2019-12-20 16:46:57 +0100

    Rewrite path to query string

--- src/index.php
+++ src/index.php
@@ -1,2 +1,34 @@
 <?php
-echo (isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']);
+
+class IpToolbox
+{
+  public $ip;
+
+  function set_ip($ip) { $this->ip = $ip; }
+
+  function Ip() { echo $this->ip; }
+
+  function Json() {
+    $json = ['ip' => $this->ip];
+    header('Content-Type: application/json');
+    echo json_encode($json);
+  }
+}
+
+$iptoolbox = new IpToolbox;
+
+$ip =(isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']);
+$iptoolbox->set_ip($ip);
+
+if (isset($_GET['q1'])) {
+  switch ($_GET['q1']) {
+    case 'json':
+      $iptoolbox->Json();
+      break;
+    default:
+      $iptoolbox->Ip();
+      break;
+  }
+} else {
+  $iptoolbox->Ip();
+}
--- src/json.php
+++
@@ -1,7 +0,0 @@
-<?php
-$ip =(isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']);
-
-$json = ['ip' => $ip];
-
-header('Content-Type: application/json');
-echo json_encode($json);
