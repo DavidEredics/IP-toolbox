5bf8ec00d15b4a771d7599ec6a7f7767f1129bd3
Dávid <mail@davideredics.com>
2019-12-21 21:49:13 +0100

    respond with one ip by default if multiple ips to get all ips use /ips or /json/ips

--- src/index.php
+++ src/index.php
@@ -3,16 +3,35 @@
 class IpToolbox
 {
   public $ip;
+  public $ips;
 
-  function set_ip($ip) { $this->ip = $ip; }
+  function set_ip($ip) {
+    if (strstr($ip, ', ')) {
+      $ips = explode(', ', $ip);
+      $ip = $ips[0];
+      $this->ips = $ips;
+    } else {
+      $this->ips = $ip;
+    }
+    $this->ip = $ip;
+  }
 
   function Ip() {
     header('Content-Type: text/plain');
     echo $this->ip;
   }
+  function Ips() {
+    header('Content-Type: text/plain');
+    echo implode(", ", $this->ips);
+  }
 
   function Json() {
     $json = ['ip' => $this->ip];
+    header('Content-Type: application/json');
+    echo json_encode($json);
+  }
+  function JsonIps() {
+    $json = ['ip' => $this->ips];
     header('Content-Type: application/json');
     echo json_encode($json);
   }
@@ -26,7 +45,21 @@ $iptoolbox->set_ip($ip);
 if (isset($_GET['q1'])) {
   switch ($_GET['q1']) {
     case 'json':
-      $iptoolbox->Json();
+      if (isset($_GET['q2'])) {
+        switch ($_GET['q2']) {
+          case 'ips':
+            $iptoolbox->JsonIps();
+            break;
+          default:
+            $iptoolbox->Json();
+            break;
+        }
+      } else {
+        $iptoolbox->Json();
+      }
+      break;
+    case 'ips':
+      $iptoolbox->Ips();
       break;
     default:
       $iptoolbox->Ip();
